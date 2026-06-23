c033cf3a5e652fe454b844da04cc18f5394c9dae
Kalesh Singh <kaleshsingh@google.com>
2026-06-23 06:06:36 +0000

    mm: Scale vma_address calculations to dynamic page size
    Under CONFIG_ARM64_PER_PROCESS_PAGE_SIZE, address mapping translations
    inside vma_address() must adapt dynamically to process-specific
    translation granule configurations.
    
    Update vma_address() in mm/internal.h to resolve the target virtual
    address using dynamic page and slice offsets. Scale page index
    boundaries using vma_page_shift() and vma_slice_shift(). For compat
    processes, handle offset scaling for anonymous mappings using shift
    differences, and adjust file-backed mappings using PAGE_SHIFT_COMPAT and
    vma_slice_off(). Include <linux/ppps.h> to resolve the dynamic paging
    helper macros.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/internal.h
+++ mm/internal.h
@@ -963,14 +963,32 @@ static inline unsigned long vma_address(struct vm_area_struct *vma,
 		pgoff_t pgoff, unsigned long nr_pages)
 {
 	unsigned long address;
+	unsigned int shift = vma_page_shift(vma);
+	unsigned int scale_shift = vma_slice_shift(vma);
+
+#ifdef CONFIG_ARM64_PER_PROCESS_PAGE_SIZE
+	if (vma->vm_mm && vma->vm_mm->page_shift == PAGE_SHIFT_COMPAT &&
+	    vma_is_anonymous((struct vm_area_struct *)vma)) {
+		pgoff = pgoff << (PAGE_SHIFT - vma->vm_mm->page_shift);
+	}
+#endif
 
 	if (pgoff >= vma->vm_pgoff) {
 		address = vma->vm_start +
-			((pgoff - vma->vm_pgoff) << PAGE_SHIFT);
+			((pgoff - vma->vm_pgoff) << shift);
+		if (ppps_mm_is_compat(vma->vm_mm) &&
+		    !vma_is_anonymous((struct vm_area_struct *)vma))
+			address -= ((unsigned long)vma_slice_off(vma) << PAGE_SHIFT_COMPAT);
 		/* Check for address beyond vma (or wrapped through 0?) */
-		if (address < vma->vm_start || address >= vma->vm_end)
+		if (address < vma->vm_start) {
+			if (ppps_mm_is_compat(vma->vm_mm) && pgoff == vma->vm_pgoff)
+				address = vma->vm_start;
+			else
+				address = -EFAULT;
+		} else if (address >= vma->vm_end) {
 			address = -EFAULT;
-	} else if (pgoff + nr_pages - 1 >= vma->vm_pgoff) {
+		}
+	} else if (pgoff + (nr_pages << scale_shift) - 1 >= vma->vm_pgoff) {
 		/* Test above avoids possibility of wrap to 0 on 32-bit */
 		address = vma->vm_start;
 	} else {
