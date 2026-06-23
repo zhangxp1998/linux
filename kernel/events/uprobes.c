5433fa7e0288b46c64e47a514514964258d42b1e
Kalesh Singh <kaleshsingh@google.com>
2026-06-23 05:58:02 +0000

    uprobes: Scale virtual address translations to dynamic page size
    Under CONFIG_ARM64_PER_PROCESS_PAGE_SIZE, virtual address translations
    and file offset conversions for uprobe instruction breakpoints must
    scale based on the target process translation granule configuration.
    
    Update uprobe tracking and validation routines inside
    kernel/events/uprobes.c. Scale page offset calculations inside
    offset_to_vaddr(), vaddr_to_offset(), unapply_uprobe(), and
    find_active_uprobe_speculative() using vma_file_offset(vma) to support
    dynamic subpage slice offsets. Validate instruction virtual addresses in
    uprobe_write() using MM_PAGE_MASK(vma->vm_mm). Initialize MMU notifier
    range intervals using MM_PAGE_SIZE(mm). Include <linux/ppps.h> to
    resolve the dynamic paging helper macros.
    
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -26,6 +26,7 @@
 #include <linux/task_work.h>
 #include <linux/shmem_fs.h>
 #include <linux/khugepaged.h>
+#include <linux/ppps.h>
 
 #include <linux/uprobes.h>
 
@@ -137,12 +138,12 @@ static bool valid_vma(struct vm_area_struct *vma, bool is_register)
 
 static unsigned long offset_to_vaddr(struct vm_area_struct *vma, loff_t offset)
 {
-	return vma->vm_start + offset - ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
+	return vma->vm_start + offset - vma_file_offset(vma);
 }
 
 static loff_t vaddr_to_offset(struct vm_area_struct *vma, unsigned long vaddr)
 {
-	return ((loff_t)vma->vm_pgoff << PAGE_SHIFT) + (vaddr - vma->vm_start);
+	return vma_file_offset(vma) + (vaddr - vma->vm_start);
 }
 
 /**
@@ -169,7 +170,7 @@ static int __replace_page(struct vm_area_struct *vma, unsigned long addr,
 	struct mmu_notifier_range range;
 
 	mmu_notifier_range_init(&range, MMU_NOTIFY_CLEAR, 0, mm, addr,
-				addr + PAGE_SIZE);
+				addr + MM_PAGE_SIZE(mm));
 
 	if (new_page) {
 		new_folio = page_folio(new_page);
@@ -527,7 +528,8 @@ retry:
 
 		VM_BUG_ON_PAGE(!PageAnon(old_page), old_page);
 
-		index = vaddr_to_offset(vma, vaddr & PAGE_MASK) >> PAGE_SHIFT;
+		index = vaddr_to_offset(vma, vaddr & MM_PAGE_MASK(mm)) >>
+			PAGE_SHIFT;
 		orig_page = find_get_page(vma->vm_file->f_inode->i_mapping,
 					  index);
 
@@ -545,7 +547,8 @@ retry:
 		}
 	}
 
-	ret = __replace_page(vma, vaddr & PAGE_MASK, old_page, new_page);
+	ret = __replace_page(vma, vaddr & MM_PAGE_MASK(mm), old_page,
+			     new_page);
 	if (new_page)
 		put_page(new_page);
 put_old:
@@ -1274,7 +1277,7 @@ static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 		    file_inode(vma->vm_file) != uprobe->inode)
 			continue;
 
-		offset = (loff_t)vma->vm_pgoff << PAGE_SHIFT;
+		offset = vma_file_offset(vma);
 		if (uprobe->offset <  offset ||
 		    uprobe->offset >= offset + vma->vm_end - vma->vm_start)
 			continue;
