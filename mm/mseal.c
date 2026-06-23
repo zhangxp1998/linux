f5ceb693dc228acb9a71f9e92d4f27ff4b9a95d9
Kalesh Singh <kaleshsingh@google.com>
2026-06-23 04:52:39 +0000

    mm: Check start address alignment dynamically in mseal
    Under CONFIG_ARM64_PER_PROCESS_PAGE_SIZE, process memory sealing
    (mseal) offset alignment checks must dynamically scale based on the
    active process translation granule configuration.
    
    Update do_mseal() inside mm/mseal.c to validate the start address
    alignment using MM_PAGE_ALIGNED() and align the request length using
    MM_PAGE_ALIGN() rather than static host PAGE_SIZE constants. Include
    <linux/ppps.h> to resolve the dynamic paging helper macros.
    
    Change-Id: Idf426951c297f13a52e7467de0ad7e021c156cc6
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/mseal.c
+++ mm/mseal.c
@@ -14,6 +14,7 @@
 #include <linux/page_size_compat.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
+#include <linux/ppps.h>
 #include "internal.h"
 
 /*
@@ -148,10 +149,17 @@ int do_mseal(unsigned long start, size_t len_in, unsigned long flags)
 		return -EINVAL;
 
 	start = untagged_addr(start);
+#ifdef CONFIG_ARM64_PER_PROCESS_PAGE_SIZE
+	if (!MM_PAGE_ALIGNED(mm, start))
+		return -EINVAL;
+
+	len = MM_PAGE_ALIGN(mm, len_in);
+#else
 	if (!__PAGE_ALIGNED(start))
 		return -EINVAL;
 
 	len = __PAGE_ALIGN(len_in);
+#endif
 	/* Check to see whether len was rounded up from small -ve to zero. */
 	if (len_in && !len)
 		return -EINVAL;
