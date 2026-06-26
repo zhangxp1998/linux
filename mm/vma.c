3f1579dff06a362046872988a33fe61d3a6da339
Kalesh Singh <kaleshsingh@google.com>
2026-06-26 23:23:06 +0000

    mm/vma: Scale stack growth and VMA insertion boundaries
    Scale the stack expansion limits in expand_upwards() and
    expand_downwards(), the stack growth accounting in
    acct_stack_growth(), and the VMA insertion page charge in
    insert_vm_struct() to use dynamic MM_PAGE_SHIFT and MM_PAGE_MASK.
    
    Change-Id: I7c21b3a6839efad002f3ab1f150775dfb6b7b37f
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/vma.c
+++ mm/vma.c
@@ -3034,7 +3034,8 @@ static int acct_stack_growth(struct vm_area_struct *vma,
 		return -ENOMEM;
 
 	/* mlock limit tests */
-	if (!mlock_future_ok(mm, vma->vm_flags, grow << PAGE_SHIFT))
+	if (!mlock_future_ok(mm, vma->vm_flags,
+			     grow << MM_PAGE_SHIFT(mm)))
 		return -ENOMEM;
 
 	/* Check to ensure the stack will not grow into a hugetlb-only region */
@@ -3072,10 +3073,10 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 	mmap_assert_write_locked(mm);
 
 	/* Guard against exceeding limits of the address space. */
-	address &= PAGE_MASK;
-	if (address >= (TASK_SIZE & PAGE_MASK))
+	address &= MM_PAGE_MASK(mm);
+	if (address >= (TASK_SIZE & MM_PAGE_MASK(mm)))
 		return -ENOMEM;
-	address += PAGE_SIZE;
+	address += MM_PAGE_SIZE(mm);
 
 	/* Enforce stack_guard_gap */
 	gap_addr = address + stack_guard_gap;
@@ -3114,10 +3115,11 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 		unsigned long size, grow;
 
 		size = address - vma->vm_start;
-		grow = (address - vma->vm_end) >> PAGE_SHIFT;
+		grow = (address - vma->vm_end) >> MM_PAGE_SHIFT(mm);
 
 		error = -ENOMEM;
-		if (vma->vm_pgoff + (size >> PAGE_SHIFT) >= vma->vm_pgoff) {
+		if (vma->vm_pgoff + (size >> MM_PAGE_SHIFT(mm)) >=
+		    vma->vm_pgoff) {
 			error = acct_stack_growth(vma, size, grow);
 			if (!error) {
 				if (vma->vm_flags & VM_LOCKED)
@@ -3156,7 +3158,7 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 
 	mmap_assert_write_locked(mm);
 
-	address &= __PAGE_MASK;
+	address &= MM_PAGE_MASK(mm);
 	if (address < mmap_min_addr || address < FIRST_USER_ADDRESS)
 		return -EPERM;
 
@@ -3193,7 +3195,7 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 		unsigned long size, grow;
 
 		size = vma->vm_end - address;
-		grow = (vma->vm_start - address) >> PAGE_SHIFT;
+		grow = (vma->vm_start - address) >> MM_PAGE_SHIFT(mm);
 
 		error = -ENOMEM;
 		if (grow <= vma->vm_pgoff) {
@@ -3205,6 +3207,7 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 				anon_vma_interval_tree_pre_update_vma(vma);
 				vma->vm_start = address;
 				vma->vm_pgoff -= grow;
+				vma_set_slice_off(vma, 0);
 				/* Overwrite old entry in mtree. */
 				vma_iter_store_overwrite(&vmi, vma);
 				anon_vma_interval_tree_post_update_vma(vma);
@@ -3243,7 +3246,7 @@ int __vm_munmap(unsigned long start, size_t len, bool unlock)
  */
 int insert_vm_struct(struct mm_struct *mm, struct vm_area_struct *vma)
 {
-	unsigned long charged = vma_pages(vma);
+	unsigned long charged = vma_nr_slices(vma);
 
 
 	if (find_vma_intersection(mm, vma->vm_start, vma->vm_end))
@@ -3267,7 +3270,7 @@ int insert_vm_struct(struct mm_struct *mm, struct vm_area_struct *vma)
 	 */
 	if (vma_is_anonymous(vma)) {
 		BUG_ON(vma->anon_vma);
-		vma->vm_pgoff = vma->vm_start >> PAGE_SHIFT;
+		vma->vm_pgoff = vma->vm_start >> MM_PAGE_SHIFT(mm);
 	}
 
 	if (vma_link(mm, vma)) {
