852fd441051f22bae09adc280e239fc8af8f24d0
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-18 10:20:09 +0000

    mm: validate PPPS slice state after userfaultfd retry

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -304,6 +304,7 @@ CATEGORY="gup_test" run_test ./gup_test -ct -F 0x1 0 19 0x1000
 CATEGORY="gup_test" run_test ./gup_longterm
 
 CATEGORY="userfaultfd" run_test ./uffd-unit-tests
+CATEGORY="userfaultfd" run_test ./userfaultfd_retry_ppps
 CATEGORY="userfaultfd" run_test ./userfaultfd_shmem_ppps
 CATEGORY="userfaultfd" run_test ./userfaultfd_zeropage_ppps
 uffd_stress_bin=./uffd-stress
