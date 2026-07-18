1a605adf50e90cb3ccfa55006c1f94f43323aa50
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-18 12:37:08 +0000

    mm: make vm_map_pages support PPPS slices

---
+++ tools/testing/selftests/mm/vm_map_pages_ppps.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef __SELFTEST_MM_VM_MAP_PAGES_PPPS_H
+#define __SELFTEST_MM_VM_MAP_PAGES_PPPS_H
+
+#define VM_MAP_PAGES_PPPS_DEVICE_NAME "vm_map_pages_ppps"
+
+#endif
