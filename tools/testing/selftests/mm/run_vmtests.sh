f72910b7a0fd979c82b79fcf4b13d7fb301795f6
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-18 08:41:46 +0000

    mm: make process_vm access honor target page size

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -388,6 +388,7 @@ CATEGORY="madv_populate" run_test ./madvise_remove_ppps
 CATEGORY="madv_populate" run_test ./madvise_willneed_ppps "$TMPDIR/madvise-willneed-ppps.bin"
 
 CATEGORY="process_madv" run_test ./process_madvise_ppps
+CATEGORY="process_madv" run_test ./process_vm_access_ppps
 
 if [ -x ./memfd_secret ]
 then
