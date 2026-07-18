3360334a728f0d4f07ba4ca14d3b3da8083d7c82
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-18 17:44:17 +0000

    fs: honor process page size in ELF core dumps
    A 4K process on a 16K PPPS kernel can have core PT_LOAD segments whose file offsets are not congruent with virtual addresses modulo the native ELF alignment. Align the core data and advertise PT_LOAD alignment using the process page size, while retaining any larger architecture ELF alignment.

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -2055,7 +2055,8 @@ static int elf_core_dump(struct coredump_params *cprm)
 		offset += sz;
 	}
 
-	dataoff = offset = roundup(offset, ELF_EXEC_PAGESIZE);
+	offset = roundup(offset, ELF_MIN_ALIGN);
+	dataoff = offset;
 
 	offset += cprm->vma_data_size;
 	offset += elf_core_extra_data_size(cprm);
@@ -2095,7 +2096,7 @@ static int elf_core_dump(struct coredump_params *cprm)
 			phdr.p_flags |= PF_W;
 		if (meta->flags & VM_EXEC)
 			phdr.p_flags |= PF_X;
-		phdr.p_align = ELF_EXEC_PAGESIZE;
+		phdr.p_align = ELF_MIN_ALIGN;
 
 		if (!dump_emit(cprm, &phdr, sizeof(phdr)))
 			goto end_coredump;
