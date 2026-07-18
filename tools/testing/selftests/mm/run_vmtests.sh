9940bbe4ed98bf3e75692e04620e001c55456aa6
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-18 10:15:08 +0000

    mm: fix PPPS userfaultfd zeropage handling

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -332,6 +332,7 @@ CATEGORY="gup_test" run_test ./gup_longterm
 
 CATEGORY="userfaultfd" run_test ./uffd-unit-tests
 CATEGORY="userfaultfd" run_test ./userfaultfd_shmem_ppps
+CATEGORY="userfaultfd" run_test ./userfaultfd_zeropage_ppps
 uffd_stress_bin=./uffd-stress
 CATEGORY="userfaultfd" run_test ${uffd_stress_bin} anon 20 16
 # Hugetlb tests require source and destination huge pages. Pass in almost half
