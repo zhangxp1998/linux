396d90617007c11769811019e1b05a9f2002d4e3
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-19 13:29:01 +0000

    proc: report VmPin in native page units
    pinned_vm counts native backing pages, but task_mem() formats it using the process page shift. This underreports pins for a 4K PPPS process on a 16K kernel. Keep process-page formatting for VMA counters and print VmPin using native PAGE_SHIFT. Extend the io_uring fixed-buffer regression to verify both accounting and unaccounting.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -70,7 +70,10 @@ void task_mem(struct seq_file *m, struct mm_struct *mm)
 	SEQ_PUT_DEC("VmPeak:\t", hiwater_vm);
 	SEQ_PUT_DEC(" kB\nVmSize:\t", total_vm);
 	SEQ_PUT_DEC(" kB\nVmLck:\t", mm->locked_vm);
-	SEQ_PUT_DEC(" kB\nVmPin:\t", atomic64_read(&mm->pinned_vm));
+	seq_put_decimal_ull_width(m,
+				  " kB\nVmPin:\t",
+				  atomic64_read(&mm->pinned_vm) <<
+				  (PAGE_SHIFT - 10), 8);
 	SEQ_PUT_DEC(" kB\nVmHWM:\t", hiwater_rss);
 	SEQ_PUT_DEC(" kB\nVmRSS:\t", total_rss);
 	SEQ_PUT_DEC(" kB\nRssAnon:\t", anon);
