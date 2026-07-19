f9e6cee11a6da906fdd4594cc2200eb96590c930
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-19 05:32:17 +0000

    media: videobuf2: hide PPPS DMA-BUF allocation padding
    Change-Id: I356a2fa2ddf36aa6502061007f1d4bf1ddee1e0c

--- drivers/media/common/videobuf2/videobuf2-dma-sg.c
+++ drivers/media/common/videobuf2/videobuf2-dma-sg.c
@@ -524,9 +524,21 @@ static struct dma_buf *vb2_dma_sg_get_dmabuf(struct vb2_buffer *vb,
 {
 	struct vb2_dma_sg_buf *buf = buf_priv;
 	struct dma_buf *dbuf;
+	unsigned long size = 0;
+	unsigned int plane;
 	DEFINE_DMA_BUF_EXPORT_INFO(exp_info);
 
+	for (plane = 0; plane < vb->num_planes; plane++) {
+		if (vb->planes[plane].mem_priv == buf_priv) {
+			size = vb2_plane_size(vb, plane);
+			break;
+		}
+	}
+	if (WARN_ON(!size || size > buf->size))
+		return NULL;
+
 	exp_info.ops = &vb2_dma_sg_dmabuf_ops;
+	/* DMA-BUF importers require a native-page-aligned allocation size. */
 	exp_info.size = buf->size;
 	exp_info.flags = flags;
 	exp_info.priv = buf;
