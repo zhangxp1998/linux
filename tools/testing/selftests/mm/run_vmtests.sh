53f51828bb0571615c19a2a7e5f9412b28018167
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-19 09:11:09 +0000

    selftests/mm: cover MADV_FREE with PPPS

--- tools/testing/selftests/mm/run_vmtests.sh
+++ tools/testing/selftests/mm/run_vmtests.sh
@@ -294,6 +294,7 @@ CATEGORY="mmap" run_test ./io_uring_mmap_ppps
 CATEGORY="mmap" run_test ./io_uring_fixed_buffer_ppps
 CATEGORY="mmap" run_test ./mglru_ppps
 CATEGORY="mmap" run_test ./anon_pageout_ppps
+CATEGORY="mmap" run_test ./madvise_free_ppps
 CATEGORY="mmap" run_test ./thp_mmap_offset_ppps
 CATEGORY="mmap" run_test ./shmem_mmap_offset_ppps
 CATEGORY="mmap" run_test ./secretmem_eof_ppps
