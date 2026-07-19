8442bdab8885fca95c014965591da70d86ee156f
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-19 04:41:55 +0000

    ALSA: pcm: bound PPPS mmap to process-page buffer size
    Update sound/core/pcm_native.c,
    tools/testing/selftests/alsa/snd_pcm_mmap_ppps.c.
    
    Change-Id: Iaab4c5dd66e0d5e3f607b52978864f9efcd0cc82

--- tools/testing/selftests/alsa/snd_pcm_mmap_ppps.c
+++ tools/testing/selftests/alsa/snd_pcm_mmap_ppps.c
@@ -10,13 +10,9 @@
 
 #include "kselftest_ppps.h"
 
-#ifndef ADDR_4KB_COMPAT_PAGE_SIZE
-#define ADDR_4KB_COMPAT_PAGE_SIZE 0x10000000
-#endif
-
-#define USER_PAGE_SIZE 4096UL
-#define MMAP_OFFSET (3 * USER_PAGE_SIZE)
-#define MMAP_SIZE (2 * USER_PAGE_SIZE)
+#define MMAP_OFFSET (3 * PROCESS_PAGE_SIZE)
+#define MMAP_SIZE (2 * PROCESS_PAGE_SIZE)
+#define TAIL_OFFSET (2 * PROCESS_PAGE_SIZE)
 
 static int run_test(void)
 {
@@ -26,9 +22,7 @@ static int run_test(void)
 
 	ksft_print_header();
 	ksft_set_plan(3);
-	ksft_test_result(sysconf(_SC_PAGESIZE) == USER_PAGE_SIZE,
-			 "process uses 4K pages\n");
-	fd = open("/dev/snd_pcm_mmap_ppps", O_RDWR | O_CLOEXEC);
+	fd = ppps_open_fixture_or_skip("/dev/snd_pcm_mmap_ppps", O_RDWR);
 	ksft_test_result(fd >= 0, "open the PCM mmap test device\n");
 
 	errno = 0;
@@ -39,6 +33,16 @@ static int run_test(void)
 			 "reject a PCM mapping that crosses the DMA buffer end\n");
 	if (mapping != MAP_FAILED)
 		munmap(mapping, MMAP_SIZE);
+
+	errno = 0;
+	mapping = mmap(NULL, PROCESS_PAGE_SIZE, PROT_READ | PROT_WRITE,
+		       MAP_SHARED, fd, TAIL_OFFSET);
+	saved_errno = errno;
+	ksft_test_result(mapping == MAP_FAILED && saved_errno == EINVAL,
+			 "reject the first process page beyond the DMA buffer (errno=%d)\n",
+			 saved_errno);
+	if (mapping != MAP_FAILED)
+		munmap(mapping, PROCESS_PAGE_SIZE);
 	close(fd);
 	ksft_finished();
 }
