1768783142769d2f20b71c2d0f669bec8f5f0316
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-20 16:35:36 +0000

    KVM: allow process-page-sized dirty rings
    KVM currently requires the dirty-ring size to be at least PAGE_SIZE. On a 16K PPPS kernel, that rejects a 4K ring requested by a 4K userspace VMM even though the ring mmap path already supports process-page slices.
    
    Use the calling process page size for the minimum ring size. Tighten the PPPS KVM regression test to create, map, and exercise a 4K dirty ring.

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -5004,9 +5004,10 @@ static int kvm_vm_ioctl_enable_dirty_log_ring(struct kvm *kvm, u32 size)
 	if (!size || (size & (size - 1)))
 		return -EINVAL;
 
-	/* Should be bigger to keep the reserved entries, or a page */
+	/* Should be bigger to keep the reserved entries, or a process page */
 	if (size < kvm_dirty_ring_get_rsvd_entries(kvm) *
-	    sizeof(struct kvm_dirty_gfn) || size < PAGE_SIZE)
+	    sizeof(struct kvm_dirty_gfn) ||
+	    size < MM_PAGE_SIZE(current->mm))
 		return -EINVAL;
 
 	if (size > KVM_DIRTY_RING_MAX_ENTRIES *
