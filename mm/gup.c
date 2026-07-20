aa4ec6264cdcaa6e2568f4f87dfec8007899c26e
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-20 18:02:49 +0000

    perf: report PPPS physical sample addresses correctly
    PERF_SAMPLE_PHYS_ADDR pins the backing page with IRQ-safe GUP and then derives the byte address from the user virtual address modulo the native PAGE_SIZE. A 4K process on a 16K kernel can map a physical slice whose offset does not match that virtual offset, so aliases of the same file byte are reported as different physical addresses.\n\nTeach the fast GUP walk to optionally return the physical offset encoded in the stable PTE and combine it with the process-page offset. Use that exact offset for perf samples. Add a regression test that samples minor faults through two differently aligned aliases of the same file byte.

--- mm/gup.c
+++ mm/gup.c
@@ -2865,7 +2865,7 @@ static bool gup_fast_folio_allowed(struct folio *folio, unsigned int flags)
  */
 static int gup_fast_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	int ret = 0;
 	pte_t *ptep, *ptem;
@@ -2930,6 +2930,8 @@ static int gup_fast_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 		}
 		folio_set_referenced(folio);
 		pages[*nr] = page;
+		if (page_offsets)
+			page_offsets[*nr] = pte_page_offset(pte);
 		(*nr)++;
 	} while (ptep++, addr += MM_PAGE_SIZE(PGTABLE_MM()), addr != end);
 
@@ -2952,7 +2954,7 @@ pte_unmap:
  */
 static int gup_fast_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	return 0;
 }
@@ -2960,7 +2962,7 @@ static int gup_fast_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 
 static int gup_fast_pmd_leaf(pmd_t orig, pmd_t *pmdp, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	struct page *page;
 	struct folio *folio;
@@ -2994,16 +2996,21 @@ static int gup_fast_pmd_leaf(pmd_t orig, pmd_t *pmdp, unsigned long addr,
 	}
 
 	pages += *nr;
+	if (page_offsets)
+		page_offsets += *nr;
 	*nr += refs;
-	for (; refs; refs--)
+	for (; refs; refs--) {
 		*(pages++) = page++;
+		if (page_offsets)
+			*(page_offsets++) = 0;
+	}
 	folio_set_referenced(folio);
 	return 1;
 }
 
 static int gup_fast_pud_leaf(pud_t orig, pud_t *pudp, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	struct page *page;
 	struct folio *folio;
@@ -3038,16 +3045,21 @@ static int gup_fast_pud_leaf(pud_t orig, pud_t *pudp, unsigned long addr,
 	}
 
 	pages += *nr;
+	if (page_offsets)
+		page_offsets += *nr;
 	*nr += refs;
-	for (; refs; refs--)
+	for (; refs; refs--) {
 		*(pages++) = page++;
+		if (page_offsets)
+			*(page_offsets++) = 0;
+	}
 	folio_set_referenced(folio);
 	return 1;
 }
 
 static int gup_fast_pmd_range(pud_t *pudp, pud_t pud, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	unsigned long next;
 	pmd_t *pmdp;
@@ -3066,11 +3078,11 @@ static int gup_fast_pmd_range(pud_t *pudp, pud_t pud, unsigned long addr,
 				return 0;
 
 			if (!gup_fast_pmd_leaf(pmd, pmdp, addr, next, flags,
-				pages, nr))
+				pages, page_offsets, nr))
 				return 0;
 
 		} else if (!gup_fast_pte_range(pmd, pmdp, addr, next, flags,
-					       pages, nr))
+					       pages, page_offsets, nr))
 			return 0;
 	} while (pmdp++, addr = next, addr != end);
 
@@ -3079,7 +3091,7 @@ static int gup_fast_pmd_range(pud_t *pudp, pud_t pud, unsigned long addr,
 
 static int gup_fast_pud_range(p4d_t *p4dp, p4d_t p4d, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	unsigned long next;
 	pud_t *pudp;
@@ -3093,10 +3105,10 @@ static int gup_fast_pud_range(p4d_t *p4dp, p4d_t p4d, unsigned long addr,
 			return 0;
 		if (unlikely(pud_leaf(pud))) {
 			if (!gup_fast_pud_leaf(pud, pudp, addr, next, flags,
-					       pages, nr))
+					       pages, page_offsets, nr))
 				return 0;
 		} else if (!gup_fast_pmd_range(pudp, pud, addr, next, flags,
-					       pages, nr))
+					       pages, page_offsets, nr))
 			return 0;
 	} while (pudp++, addr = next, addr != end);
 
@@ -3105,7 +3117,7 @@ static int gup_fast_pud_range(p4d_t *p4dp, p4d_t p4d, unsigned long addr,
 
 static int gup_fast_p4d_range(pgd_t *pgdp, pgd_t pgd, unsigned long addr,
 		unsigned long end, unsigned int flags, struct page **pages,
-		int *nr)
+		unsigned long *page_offsets, int *nr)
 {
 	unsigned long next;
 	p4d_t *p4dp;
@@ -3119,7 +3131,7 @@ static int gup_fast_p4d_range(pgd_t *pgdp, pgd_t pgd, unsigned long addr,
 			return 0;
 		BUILD_BUG_ON(p4d_leaf(p4d));
 		if (!gup_fast_pud_range(p4dp, p4d, addr, next, flags,
-					pages, nr))
+					pages, page_offsets, nr))
 			return 0;
 	} while (p4dp++, addr = next, addr != end);
 
@@ -3127,7 +3139,8 @@ static int gup_fast_p4d_range(pgd_t *pgdp, pgd_t pgd, unsigned long addr,
 }
 
 static void gup_fast_pgd_range(unsigned long addr, unsigned long end,
-		unsigned int flags, struct page **pages, int *nr)
+		unsigned int flags, struct page **pages,
+		unsigned long *page_offsets, int *nr)
 {
 	unsigned long next;
 	pgd_t *pgdp;
@@ -3141,13 +3154,14 @@ static void gup_fast_pgd_range(unsigned long addr, unsigned long end,
 			return;
 		BUILD_BUG_ON(pgd_leaf(pgd));
 		if (!gup_fast_p4d_range(pgdp, pgd, addr, next, flags,
-					pages, nr))
+					pages, page_offsets, nr))
 			return;
 	} while (pgdp++, addr = next, addr != end);
 }
 #else
 static inline void gup_fast_pgd_range(unsigned long addr, unsigned long end,
-		unsigned int flags, struct page **pages, int *nr)
+		unsigned int flags, struct page **pages,
+		unsigned long *page_offsets, int *nr)
 {
 }
 #endif /* CONFIG_HAVE_GUP_FAST */
@@ -3164,7 +3178,8 @@ static bool gup_fast_permitted(unsigned long start, unsigned long end)
 #endif
 
 static unsigned long gup_fast(unsigned long start, unsigned long end,
-		unsigned int gup_flags, struct page **pages)
+		unsigned int gup_flags, struct page **pages,
+		unsigned long *page_offsets)
 {
 	unsigned long flags;
 	int nr_pinned = 0;
@@ -3191,7 +3206,8 @@ static unsigned long gup_fast(unsigned long start, unsigned long end,
 	 * that come from callers of tlb_remove_table_sync_one().
 	 */
 	local_irq_save(flags);
-	gup_fast_pgd_range(start, end, gup_flags, pages, &nr_pinned);
+	gup_fast_pgd_range(start, end, gup_flags, pages, page_offsets,
+			   &nr_pinned);
 	local_irq_restore(flags);
 
 	/*
@@ -3210,7 +3226,8 @@ static unsigned long gup_fast(unsigned long start, unsigned long end,
 }
 
 static int gup_fast_fallback(unsigned long start, unsigned long nr_pages,
-		unsigned int gup_flags, struct page **pages)
+		unsigned int gup_flags, struct page **pages,
+		unsigned long *page_offsets)
 {
 	unsigned long len, end;
 	unsigned long nr_pinned;
@@ -3236,7 +3253,7 @@ static int gup_fast_fallback(unsigned long start, unsigned long nr_pages,
 	if (end > TASK_SIZE_MAX)
 		return -EFAULT;
 
-	nr_pinned = gup_fast(start, end, gup_flags, pages);
+	nr_pinned = gup_fast(start, end, gup_flags, pages, page_offsets);
 	if (nr_pinned == nr_pages || gup_flags & FOLL_FAST_ONLY)
 		return nr_pinned;
 
@@ -3290,9 +3307,27 @@ int get_user_pages_fast_only(unsigned long start, int nr_pages,
 			       FOLL_GET | FOLL_FAST_ONLY))
 		return -EINVAL;
 
-	return gup_fast_fallback(start, nr_pages, gup_flags, pages);
+	return gup_fast_fallback(start, nr_pages, gup_flags, pages, NULL);
 }
 EXPORT_SYMBOL_GPL(get_user_pages_fast_only);
+
+bool get_user_page_fast_only_with_offset(unsigned long addr,
+					 unsigned int gup_flags,
+					 struct page **pagep,
+					 unsigned long *page_offset)
+{
+	unsigned long pte_offset;
+
+	if (!page_offset ||
+	    !is_valid_gup_args(pagep, NULL, &gup_flags,
+			       FOLL_GET | FOLL_FAST_ONLY))
+		return false;
+	if (gup_fast_fallback(addr, 1, gup_flags, pagep, &pte_offset) != 1)
+		return false;
+
+	*page_offset = pte_offset + mm_offset_in_page(current->mm, addr);
+	return true;
+}
 
 /**
  * get_user_pages_fast() - pin user pages in memory
@@ -3321,7 +3356,7 @@ int get_user_pages_fast(unsigned long start, int nr_pages,
 	 */
 	if (!is_valid_gup_args(pages, NULL, &gup_flags, FOLL_GET))
 		return -EINVAL;
-	return gup_fast_fallback(start, nr_pages, gup_flags, pages);
+	return gup_fast_fallback(start, nr_pages, gup_flags, pages, NULL);
 }
 EXPORT_SYMBOL_GPL(get_user_pages_fast);
 
@@ -3349,7 +3384,7 @@ int pin_user_pages_fast(unsigned long start, int nr_pages,
 {
 	if (!is_valid_gup_args(pages, NULL, &gup_flags, FOLL_PIN))
 		return -EINVAL;
-	return gup_fast_fallback(start, nr_pages, gup_flags, pages);
+	return gup_fast_fallback(start, nr_pages, gup_flags, pages, NULL);
 }
 EXPORT_SYMBOL_GPL(pin_user_pages_fast);
 
