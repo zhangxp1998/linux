33ce38d8a4b2938113cd6842f608082f718d99a8
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-21 02:34:56 +0000

    vfio: bound group device names to process pages

--- drivers/vfio/group.c
+++ drivers/vfio/group.c
@@ -12,6 +12,7 @@
 
 #include <linux/vfio.h>
 #include <linux/iommufd.h>
+#include <linux/mm.h>
 #include <linux/anon_inodes.h>
 #include "vfio.h"
 
@@ -307,7 +308,7 @@ static int vfio_group_ioctl_get_device_fd(struct vfio_group *group,
 	char *buf;
 	int fd;
 
-	buf = strndup_user(arg, PAGE_SIZE);
+	buf = strndup_user(arg, MM_PAGE_SIZE(current->mm));
 	if (IS_ERR(buf))
 		return PTR_ERR(buf);
 
