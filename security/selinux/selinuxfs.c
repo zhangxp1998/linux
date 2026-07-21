7f524b161a6517eb7812c50407960830aaef6f64
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-21 01:01:07 +0000

    selinux: bound selinuxfs writes to process pages

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -141,7 +141,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	int scan_value;
 	bool old_value, new_value;
 
-	if (count >= PAGE_SIZE)
+	if (count >= MM_PAGE_SIZE(current->mm))
 		return -ENOMEM;
 
 	/* No partial writes. */
@@ -746,7 +746,7 @@ static ssize_t sel_write_validatetrans(struct file *file,
 		goto out;
 
 	rc = -ENOMEM;
-	if (count >= PAGE_SIZE)
+	if (count >= MM_PAGE_SIZE(current->mm))
 		goto out;
 
 	/* No partial writes. */
@@ -1250,7 +1250,7 @@ static ssize_t sel_write_bool(struct file *filep, const char __user *buf,
 	unsigned index = file_inode(filep)->i_ino & SEL_INO_MASK;
 	const char *name = filep->f_path.dentry->d_name.name;
 
-	if (count >= PAGE_SIZE)
+	if (count >= MM_PAGE_SIZE(current->mm))
 		return -ENOMEM;
 
 	/* No partial writes. */
@@ -1305,7 +1305,7 @@ static ssize_t sel_commit_bools_write(struct file *filep,
 	ssize_t length;
 	int new_value;
 
-	if (count >= PAGE_SIZE)
+	if (count >= MM_PAGE_SIZE(current->mm))
 		return -ENOMEM;
 
 	/* No partial writes. */
@@ -1437,7 +1437,7 @@ static ssize_t sel_write_avc_cache_threshold(struct file *file,
 	if (ret)
 		return ret;
 
-	if (count >= PAGE_SIZE)
+	if (count >= MM_PAGE_SIZE(current->mm))
 		return -ENOMEM;
 
 	/* No partial writes. */
