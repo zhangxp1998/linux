51001c9994ffcbbcdf1b8f85ba7efc61a014f91a
Kelvin Zhang <zhangxp1998@gmail.com>
2026-07-21 01:26:21 +0000

    tracing: bound event control writes to process pages

--- kernel/trace/trace_events.c
+++ kernel/trace/trace_events.c
@@ -1901,7 +1901,7 @@ event_filter_write(struct file *filp, const char __user *ubuf, size_t cnt,
 	char *buf;
 	int err = -ENODEV;
 
-	if (cnt >= PAGE_SIZE)
+	if (cnt >= MM_PAGE_SIZE(current->mm))
 		return -EINVAL;
 
 	buf = memdup_user_nul(ubuf, cnt);
@@ -2054,7 +2054,7 @@ subsystem_filter_write(struct file *filp, const char __user *ubuf, size_t cnt,
 	char *buf;
 	int err;
 
-	if (cnt >= PAGE_SIZE)
+	if (cnt >= MM_PAGE_SIZE(current->mm))
 		return -EINVAL;
 
 	buf = memdup_user_nul(ubuf, cnt);
