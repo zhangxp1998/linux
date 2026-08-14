098297d5b630ce6f98b499f068f2ed5ff8deb04c
Kelvin Zhang <zhangxp1998@gmail.com>
2026-08-13 19:41:19 -0700

    KVM: arm64: hide S1PIE when the host capability is disabled
    Clear ID_AA64MMFR3_EL1.S1PIE in the guest-visible sanitized feature
    register when the host does not have ARM64_HAS_S1PIE. TTBR0 and EFI
    geometry switching are already handled by the earlier MMU initialization
    change.
    
    Change-Id: I26c88c2f4502eb56b0405b7f30411072c3c6dea7
    Signed-off-by: Kelvin Zhang <zhangxp1998@gmail.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1421,6 +1421,8 @@ static u64 __kvm_read_sanitised_id_reg(const struct kvm_vcpu *vcpu,
 
 		if (!system_supports_poe())
 			val &= ~ID_AA64MMFR3_EL1_S1POE;
+		if (!cpus_have_final_cap(ARM64_HAS_S1PIE))
+			val &= ~ID_AA64MMFR3_EL1_S1PIE;
 		break;
 	case SYS_ID_MMFR4_EL1:
 		val &= ~ARM64_FEATURE_MASK(ID_MMFR4_EL1_CCIDX);
