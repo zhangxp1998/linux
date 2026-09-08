d8e93a2e8e98f5d9b4c94d98e2b674426b014d5f
Kelvin Zhang <zhangxp1998@gmail.com>
2026-09-07 22:10:02 -0700

    mm: provide a byte-offset accessor for file VMAs
    Give mmap consumers a common name for the byte offset represented by
    vm_pgoff. Convert videobuf2 and io_uring without changing their offset
    variable types, selector handling or mapping bounds.
    
    Change-Id: I0d3cd249b370a5c51408e176e5a3f61bfea14ed3
    Signed-off-by: Kelvin Zhang <zhangxp1998@gmail.com>

--- io_uring/memmap.c
+++ io_uring/memmap.c
@@ -250,7 +250,7 @@ __cold int io_uring_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct io_ring_ctx *ctx = file->private_data;
 	size_t sz = vma->vm_end - vma->vm_start;
-	long offset = vma->vm_pgoff << PAGE_SHIFT;
+	long offset = vma_file_offset(vma);
 	unsigned int npages;
 	void *ptr;
 
